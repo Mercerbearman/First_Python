fef8298351f45dfd06ef54c20fc92e7d1644f4fb
Mercerbearman <Mercerbearman@users.noreply.github.com>
2015-02-12 23:39:24 -0500

    Another one!
    Adding Eclipse files to gitignore.
    
    Using folder paths to get to subfolders.

--- First/Main.py
+++ First/Main.py
@@ -7,14 +7,14 @@ Created on Feb 9, 2015
 if __name__ == '__main__':
     #import entire class for sqLite.
     #from sqLite import sqLite_Test
-    from sqLite import *
+    from First.sqLite import *
     sq = sqLite_Test()
     
     #Grab all functions in sqLite_Test
     allSqLiteFuncts = dir(sqLite_Test)
     print('Class {0} - Interface {1}'.format(sqLite_Test, allSqLiteFuncts))
     
-    from OpenCV import OpenCV_Test
+    from First.OpenCV import OpenCV_Test
     cv = OpenCV_Test()
     
     #Grab all functions in OpenCV_Test
